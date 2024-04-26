883c5845486b273a23ce7d132a040dd5a3a13f08
MartinSilvaJose <118887247+MartinSilvaJose@users.noreply.github.com>
2024-04-26 13:18:01 +0200

    Update styles.css and index.js files

--- scripts/index.js
+++ scripts/index.js
@@ -10,3 +10,44 @@ document.addEventListener('keydown', function (event) {
 document.getElementById('upEvent').addEventListener('click', function () {
     window.scrollTo(0, 0);
 });
+
+
+// Observer API
+let section = document.querySelectorAll('.intersection');
+
+function isVisible(entries) {
+    let entry = entries[0];
+    if (entry.isIntersecting) {
+        console.log('Visible');
+        entry.target.classList.add('active');
+        setInterval(() => {
+            section[1].classList.add('active');
+        }, 1000);
+        setInterval(() => {
+            section[2].classList.add('active');
+        }, 2000);
+    }
+}
+let observer = new IntersectionObserver(isVisible, {});
+
+Array.from(section).forEach((section) => {
+    observer.observe(section);
+});
+
+
+
+// animacion para abrir el menu
+const menu = document.querySelectorAll('.borrado');
+document.querySelector('.openMenu').addEventListener('click', function () {
+    if (this.textContent === '🔻') {
+        this.textContent = '🔺';
+        menu.forEach((element) => {
+            element.classList.add('borrado');
+        });
+    } else {
+        this.textContent = '🔻';
+        Array.from(menu).forEach((element) => {
+            element.classList.remove('borrado');
+        });
+    }
+});
